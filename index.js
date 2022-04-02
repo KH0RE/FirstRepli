7002d9b317283919d92ce9ea265f88fa61dd9bcd
KH0RE <52050442+KH0RE@users.noreply.github.com>
2022-04-02 22:01:32 +0000

    new files

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import {User} from './User.js'
+import {User} from './User.js';
+import {Product} from './Product.js'
 
 const carlos = new User ('Carlos' ,'Quinaluisa');
-console.log(carlos);
+const product = new Product ('laptop', 2300);
+console.log(carlos, product);
